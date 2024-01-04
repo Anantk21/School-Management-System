6c14025d7d397d02332dd7abae707b4eccca2a76
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2024-01-05 00:07:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 app.use(express.json({ limit: '10mb' }))
 app.use(cors(
     {
-        origin: ["https://school-management-system-1whq.vercel.app"],
+        origin: ["https://school-management-system-frontend-navy.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
